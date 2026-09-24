b229b55d908010556a34bac41befd495ac9392c3
Tân Vy Phát <tanvyphatpaper@gmail.com>
2026-09-24 10:10:51 +0700

    feat(shops): add CRUD endpoints

--- backend/src/shops/shops.controller.ts
+++ backend/src/shops/shops.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateShopDto } from './dto/create-shop.dto.js';
+import { UpdateShopDto } from './dto/update-shop.dto.js';
+import { ShopsService } from './shops.service.js';
 
 @Controller('shops')
-export class ShopsController {}
+export class ShopsController {
+  constructor(private readonly shopsService: ShopsService) {}
+
+  @Post()
+  create(@Body() createShopDto: CreateShopDto) {
+    return this.shopsService.create(createShopDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.shopsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.shopsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateShopDto: UpdateShopDto,
+  ) {
+    return this.shopsService.update(id, updateShopDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.shopsService.remove(id);
+  }
+}
